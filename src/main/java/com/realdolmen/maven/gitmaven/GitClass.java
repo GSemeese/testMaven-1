5edbb150b6772ca27004837fd0bf5f55f3884571
Steven De Cock <steven_decock@hotmail.com>
2018-09-24 10:30:18 +0200

    initial commit maven project

---
+++ src/main/java/com/realdolmen/maven/gitmaven/GitClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.realdolmen.maven.gitmaven;
+
+/**
+ *
+ * @author SDOAX36
+ */
+public class GitClass {
+    
+    public void test(){
+        System.out.println("Hello Git world");
+    }
+}
